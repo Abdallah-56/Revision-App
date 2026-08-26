5047651054a0964efcd2cf07effbc8d655f73b25
Abdallah <abdallahabdisaeed@gmail.com>
2026-08-26 17:11:39 +0100

    Created Log In UI

--- Revision App/SignUpWindow.xaml.cs
+++ Revision App/SignUpWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Revision_App
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SignUpWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SignUpWindow : Window
     {
-        public MainWindow()
+        public SignUpWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
